b13c99613adb509a5ff55717464c375daab599c0
Tedus-AI <tedus.chen@gmail.com>
2026-05-03 00:33:28 +0800

    Remove initial Electron React scaffold files

--- src/renderer/components/RecordingPanel.tsx
+++
@@ -1,9 +0,0 @@
-export default function RecordingPanel() {
-  return (
-    <section>
-      <h3>Recording</h3>
-      <button type="button">Start Recording</button>
-      <button type="button">Stop Recording</button>
-    </section>
-  );
-}
